bcbb20ffe236bff186645cf0cbe569693bb903d3
Ryan Torre <rtorre1799@gmail.com>
2022-10-30 16:42:48 -0400

    added Flask sessions along with css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 from surveys import *
@@ -8,26 +8,27 @@ app.config['SECRET_KEY'] = 'test'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-# This sets up our fake database:
-responses = []
 
 @app.route('/')
 def home_page():
     """this gets the pre-made surveys that are established from the surveys.py file and displays them"""
     survey_list = surveys
+
+    """this is to set up the Flask session to capture all answers ((and reset when a user completes the survey and returns to the home page))"""
+    session["responses"] = []
     return render_template('root.html', surveys=survey_list)
 
 
 @app.route('/<survey_name>/questions/<int:question_id>', methods=["POST", "GET"])
 def question_page(survey_name, question_id):
+    responses = session["responses"]
+
     """this sets up each question from the correspond survey as well as every choice"""
     question_num = question_id
     survey_content = surveys[f"{survey_name}"]
 
     """this captures the current survey so that the redirect can occur in the /answer route"""
     current_survey = survey_name
-
-
 
     """these conditionals are what prevents the user from jumping ahead or using the wrong URL"""
     if(question_id > len(survey_content.questions) or question_id < 0):
@@ -39,8 +40,7 @@ def question_page(survey_name, question_id):
         return redirect(f"/{survey_name}/questions/{len(responses)}")
     else:
         question = survey_content.questions[question_id]
-        return render_template('question.html', q_num=question_num, question=question, responses=responses, survey_name=current_survey)
-        """TO-DO: Get rid of current_responses from above and within the render_template as we want this to occur on the backend"""
+        return render_template('question.html', q_num=question_num, question=question, survey_name=current_survey)
 
 
 
@@ -48,11 +48,11 @@ def question_page(survey_name, question_id):
 
 @app.route('/answer', methods=['POST'])
 def obtain_answer():
-    """this specifically captures the answer and then appends it to the responses list in surveys.py"""
-    answer = request.form.get("question", "not answered")
-    current_responses = responses
-    if answer != None:
-        current_responses.append(answer)
+    """this captures the users answer and then appends it."""
+    answer = request.form.get("question", "Not Answered")
+    current_responses_session = session["responses"]
+    current_responses_session.append(answer)
+    session["responses"] = current_responses_session
 
     """this sets up the redirect using hidden form data/post variables"""
     current_survey = request.form.get("current_survey")
@@ -64,15 +64,14 @@ def obtain_answer():
     if(next_question > question_count):
         return redirect('/completed')
     else:
+        """this appends the answer to the Flask session cookie and then redirects to the next question"""
         return redirect(f'/{current_survey}/questions/{next_question}')
 
 
 
 @app.route('/completed')
 def thank_you():
+    """sends user the thank you page along with the answers that the user chose"""
     return render_template('thanks.html')
 
 
-
-
-# To do: add sessions --  once that is learned.
